cb8351f19f350339788efee1bb3b56878eb4ffe7
BoesBroes <jonathan.plasman@gmail.com>
2023-01-04 14:43:44 +0100

    Some basic movement

---
+++ Assets/Scripts/Player/Movement/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveX;
+    public float moveY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * moveX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * moveY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //rotate camera
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
---
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("movement")]
+
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    [Header("ground check")]
+    public float playerHeight;
+    public LayerMask ground;
+    private bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+        SpeedControl();
+
+        //grounded check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.6f);
+
+        if (grounded)
+        {
+            rigidBody.drag = groundDrag;
+        }
+        else
+        {
+            rigidBody.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        //calculate movement
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rigidBody.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatSpeed = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z);
+
+        //limit velocity
+        if (flatSpeed.magnitude > moveSpeed)
+        {
+            Vector3 limitedSpeed = flatSpeed.normalized * moveSpeed;
+            rigidBody.velocity = new Vector3(limitedSpeed.x, rigidBody.velocity.y, limitedSpeed.z);
+        }
+
+    }
+
+}
